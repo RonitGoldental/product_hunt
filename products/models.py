37e7f4a6a45c77a758a61b5b9cabaac9fdcbb548
RonitGoldental <ronit.goldental@gmail.com>
2019-10-04 17:31:16 +0300

    products added

--- products/models.py
+++ products/models.py
@@ -1,6 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Products(models.Model):
-    name = models.CharField(max_length=64)
+    title = models.CharField(max_length=64)
+    pub_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    votes_total = models.IntegerField(default=1)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    body = models.TextField()
+    url = models.TextField()
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:10]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.utils.timezone import localtime, now
+from django.contrib.auth.decorators import login_required
+from .models import Products
+from django.http import HttpResponse
+
 
 def home(request):
-    return render(request,'home.html')
+    products = Products.objects
+    return render(request, 'home.html', {'products': products})
+
+
+@login_required(login_url="/accounts/signup/")
+def create(request):
+    if request.method == 'POST':
+        products = Products()
+        products.title = request.POST['title']
+        products.body = request.POST['body']
+        products.url = request.POST['url']
+        products.image = request.FILES['image']
+        products.icon = request.FILES['icon']
+        products.hunter = request.user
+        products.pub_date = localtime(now())
+        products.save()
+        return redirect('/products/' + str(products.id))
+    else:
+        return render(request, 'create.html')
+
+
+def detail(request, products_id):
+    products = get_object_or_404(Products, pk=products_id)
+    return render(request, 'detail.html', {'products': products})
+
+
+@login_required()
+def upvote(request, products_id):
+    if request.method == 'POST':
+        products = get_object_or_404(Products, pk=products_id)
+        products.votes_total = products.votes_total + 1
+        products.save()
+        return redirect('/products/' + str(products.id))
